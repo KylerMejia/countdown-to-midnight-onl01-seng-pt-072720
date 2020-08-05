e2f285c28b70468fa629d6ec1ef02572e73b4b2a
Kyler Mejia <kylermejia1@gmail.com>
2020-08-05 01:08:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,7 @@
-#write your code here
-
-def countdown
-end
+def countdown(number) # number is our parameter - it will hold the value of whatever argument is passed in when this method is invoked
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        # number = number - 1
+        number -= 1 # decrementing by 1
+    end
+    "HAPPY NEW YEAR!"
